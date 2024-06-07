cedaa79bace298e7b550600c91623a56d5ef1e03
Kushal Patel <kushal300603@gmail.com>
2024-06-07 17:02:16 +0530

    test: Testing if AWS Amplify deployement works

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className='w-screen h-screen bg-black'>
         <h1 className="w-full text-3xl font-bold underline text-yellow justify-center">
-          Hello world!
+          HomePage!!!
         </h1>
       </div>
     </>
